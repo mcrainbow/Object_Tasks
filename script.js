51280466ae56102863299c167acf8ae02f07feaa
Stephan Frolov <stefro97@yandex.ru>
2023-08-07 19:00:57 +0300

    Start Commit

---
+++ script.js
@@ -0,0 +1,20 @@
+"use strict";
+
+// ! Task 1
+const users = [
+  {
+    username: "David",
+    status: "online",
+    lastActivity: 10,
+  },
+  {
+    username: "Lucy",
+    status: "offline",
+    lastActivity: 22,
+  },
+  {
+    username: "Bob",
+    status: "online",
+    lastActivity: 104,
+  },
+];
